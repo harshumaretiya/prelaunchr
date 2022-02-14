1e421648f8fc400df86e64c3602f72e4b2fd5d38
Kashif Ghaffar Asim <kashif.ghaffar100@gmail.com>
2022-02-15 02:10:28 +0500

    Env Static files

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,7 +13,7 @@ Prelaunchr::Application.configure do
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
-  config.serve_static_files = false
+  config.serve_static_files = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = true
